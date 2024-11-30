668ead6096848ea468b51f08ca913ef7b1fa121e
Sootta <147506050+Sootta@users.noreply.github.com>
2024-11-30 13:38:26 +0900

    初回コミット

---
+++ src/interface.h
@@ -0,0 +1,17 @@
+#ifndef SOTA_INTERFACE_H
+#define SOTA_INTERFACE_H
+
+class Motor {
+    public:
+    void motor_rotate(int speed) {
+        if(speed < 0) {
+            this->ccw(-speed);
+        }
+        else{
+            this->cw(speed);
+        }
+    }
+    virtual void cw(int speed) = 0;
+    virtual void ccw(int speed) = 0;
+};
+#endif
